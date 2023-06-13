d2ef0ba72ccc4f84c5bcd265221811c60da4d1ed
Adriel Fabrício <afabricio@scale.com.br>
2023-06-13 15:33:31 -0300

    feat: implementada validacao de email na recuperacao de senha

--- src/pages/ForgotPassword/ForgotPassword.schema.ts
+++ src/pages/ForgotPassword/ForgotPassword.schema.ts
@@ -1,5 +1,19 @@
 import * as yup from "yup";
 
+yup.addMethod(yup.string, "email", function (errorMessage) {
+  return this.test(`test-email`, errorMessage, function (value) {
+    const { path, createError } = this;
+
+    return (
+      value?.includes("@uesc.br") ??
+      createError({ path, message: errorMessage })
+    );
+  });
+});
+
 export const ForgotPasswordSchema = yup.object().shape({
-  email: yup.string().min(8).max(10).required("Este campo é obrigatório"),
+  email: yup
+    .string()
+    .email("Email deve ser institucional.")
+    .required("Este campo é obrigatório."),
 });
