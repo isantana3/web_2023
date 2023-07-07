7512ba1ace291b16fc58a50e03584897ad378392
Adriel Fabrício <afabricio@scale.com.br>
2023-07-07 00:47:39 -0300

    feat: integrada requisição para ativação de conta

--- src/pages/NewAccess/NewAccess.page.tsx
+++ src/pages/NewAccess/NewAccess.page.tsx
@@ -1,10 +1,18 @@
+import { useEffect, useState } from "react";
+
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { authService } from "service/auth/auth.service";
+
 import colcicLogo from "assets/images/logo.png";
 import { Button } from "components/Button";
 import { Form } from "components/Form/Form";
-import { Input } from "components/Input";
+import { Input } from "components/Form/Input";
 import { Navbar } from "components/Navbar";
 
 import { NewAccessSchema } from "./NewAccess.schema";
+
+import { type IFormData, type IRequestForm } from "./NewAccess.types";
 
 import {
   CentralizedContent,
@@ -15,27 +23,51 @@ import {
 } from "./NewAccess.styles";
 
 export function NewAccess(): JSX.Element {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [token, setToken] = useState<string>("");
+
+  async function handleSubmit(data: IFormData): Promise<void> {
+    try {
+      const { password } = data;
+      const newAccessData: IRequestForm = { token, password };
+      const response = await authService.newAccess(newAccessData);
+
+      if (response.status === 201) {
+        toast.success("Conta ativada com sucesso!");
+        navigate("/");
+      }
+    } catch (error) {
+      toast("Ocorreu um erro inesperado. Tente novamente mais tarde.");
+    }
+  }
+
+  useEffect(() => {
+    const queryParamToken = searchParams.get("token") as string;
+    setToken(queryParamToken);
+  }, []);
+
   return (
     <Container>
       <Navbar />
       <CentralizedContent>
-        <Form onSubmit={(data) => {}} schema={NewAccessSchema}>
+        <Form onSubmit={handleSubmit} schema={NewAccessSchema}>
           <Header>
             <img src={colcicLogo} />
             <Title>COLCIC</Title>
           </Header>
           <SmallText>Atualize suas credenciais antes de continuar</SmallText>
           <Input
+            label=""
+            name="password"
             type="password"
             placeholder="Nova senha"
-            defaultValue={""}
-            onChange={(text) => {}}
           />
           <Input
+            label=""
+            name="confirm_password"
             type="password"
             placeholder="Confirme a senha"
-            defaultValue={""}
-            onChange={(text) => {}}
           />
 
           <Button label="Continuar" color="primary" type="submit" center />
